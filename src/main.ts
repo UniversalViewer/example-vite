2c96ec39f182312f2cb93d8f9e293de59a81b253
Demian Katz <demian.katz@villanova.edu>
2026-05-14 07:42:55 -0400

    Wire up MediaElement iconSprite (won't work until UV 4.4 is released).

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import "./style.css";
 import { init } from "universalviewer";
 import "universalviewer/dist/esm/index.css";
+import meSprite from "universalviewer/dist/mejs-controls.svg";
 
 const data = {
-  manifest: "https://wellcomelibrary.org/iiif/b18035723/manifest",
+  manifest: "https://iiif.io/api/cookbook/recipe/0003-mvm-video/manifest.json",
   embedded: true // needed for codesandbox frame
 };
 
-init("uv", data);
+const uv = init("uv", data);
+uv.on("configure", function ({ config, cb }) {
+  cb({
+    modules: { mediaElementCenterPanel: { options: { iconSprite: meSprite } } }
+  });
+});
